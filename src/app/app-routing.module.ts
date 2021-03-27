042bfb98b184333c3108dbc68bbf40d3af987ebf
unknown <ali.bouyahya@gmail.com>
2021-03-28 00:02:30 +0100

    firdt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompComponent } from './comp/comp.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'navbar',
+    component: NavigationComponent
+  },
+  {
+    path: 'footer',
+    component: CompComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
